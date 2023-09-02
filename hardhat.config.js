929204417454357bd027d39f30297392b3a83c32
solidity12 <volibear1001@gmail.com>
2023-09-02 20:49:35 +0900

    build: Added dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.PK;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [{
+      version: "0.8.9",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        }
+      }
+    }]
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        // url: "https://bsc-testnet.publicnode.com",
+        url: "https://opbnb-testnet-rpc.bnbchain.org",
+      }
+    },
+    bnb: {
+      url: "https://bsc-testnet.publicnode.com",
+      accounts: [PRIVATE_KEY],
+    },
+    opbnb: {
+      url: "https://opbnb-testnet-rpc.bnbchain.org",
+      accounts: [PRIVATE_KEY],
+    },
+  }
 };
