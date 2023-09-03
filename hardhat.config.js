029572e02d07d38c1ab4b07192d5190151b6828d
solidity12 <volibear1001@gmail.com>
2023-09-03 11:51:48 +0900

    chore: Updated hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,13 +8,13 @@ module.exports = {
   solidity: {
     compilers: [{
       version: "0.8.9",
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 200,
-        }
+    }],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       }
-    }]
+    }
   },
   networks: {
     hardhat: {
